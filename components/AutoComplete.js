29a7f90f03735a02c8c2d723573e2039d5783c89
samarth2534@gmail.com <samarth2534@gmail.com>
2025-02-08 10:55:25 +0530

    Implementing auto-complete

---
+++ components/AutoComplete.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "../App.css";
+
+const AutoComplete = () => {
+  const [results, setResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [showResults, setShowResults] = useState(false);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://dummyjson.com/recipes/search?q=" + searchInput
+    );
+    const json = await data.json();
+    console.log(json?.recipes);
+    setResults(json?.recipes);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [searchInput]);
+
+  return (
+    <div>
+      <h1>AutoComplete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="search-text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onFocus={() => setShowResults(true)}
+          onBlur={() => setShowResults(false)}
+        />
+        {showResults && (
+          <div className="results-container">
+            {results.map((r) => (
+              <span className="result" key={r?.id}>
+                {r?.name}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AutoComplete;
